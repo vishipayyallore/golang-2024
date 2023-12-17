32daba3fec0df710ca9269590152eff3164fcf33
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-17 20:39:59 +0530

    code refactoring

--- nutriscorev1/components/constants/constants.go
+++
@@ -1,4 +0,0 @@
-package constants
-
-// ScoreToLetter represents the mapping from score to letter
-var ScoreToLetter = []string{"A", "B", "C", "D", "E"}
--- nutriscorev1/main.go
+++ nutriscorev1/main.go
@@ -2,28 +2,27 @@ package main
 
 import (
 	"fmt"
-	"nutriscorev1/components/energy"
 	"nutriscorev1/score"
 )
 
 func main() {
-	fmt.Println("Nutritional score")
 
-	ns := score.GetNutritionalScore(energy.NutritionalData{
-		Energy: score.EnergyFromKcal(0),
+	fmt.Println("=========================================")
+	fmt.Println("Nutritional score")
+	fmt.Println("=========================================")
+
+	ns := score.GetNutritionalScore(score.NutritionalData{
+		Energy:              score.EnergyFromKcal(0),
+		Sugars:              score.SugarGram(10),
+		SaturatedFattyAcids: score.SaturatedFattyAcidsGram(2),
+		Sodium:              score.SodiumMilligram(500),
+		Fruits:              score.FruitsPercent(60),
+		Fibre:               score.FibreGram(4),
+		Protein:             score.ProteinGram(2),
 	}, score.Food)
 
 	fmt.Printf("Nutritional score: %d\n", ns.Value)
 	fmt.Printf("NutriScore: %s\n", ns.GetNutriScore())
 
-	// ns := score.GetNutritionalScore(components.NutritionalData{
-	// 	Energy:              components.EnergyFromKcal(0),
-	// 	Sugars:              components.SugarGram(10),
-	// 	SaturatedFattyAcids: components.SaturatedFattyAcidsGram(2),
-	// 	Sodium:              components.SodiumMilligram(500),
-	// 	Fruits:              components.FruitsPercent(60),
-	// 	Fibre:               components.FibreGram(4),
-	// 	Protein:             components.ProteinGram(2),
-	// }, components.Food)
-
+	fmt.Println("-----------------------------------------")
 }
