006bbc73f0a0d0da6abbe887d707f83192a2889c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-21 10:08:09 +0530

    Changes

---
+++ ATour/packagesdemo/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	// Packages Demo
+	fmt.Println("My favorite number is", rand.Intn(10))
+
+	// Values Demo
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
