f107b6afc2ec25bcd44b038a40b29eabaa468095
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-25 22:25:35 +0530

    Notes

--- ATour/a7functions/mathfunctions.go
+++ ATour/a7functions/mathfunctions.go
@@ -15,3 +15,14 @@ func split(sum int) (x, y int) {
 
 	return
 }
+
+/*
+Notes:
+
+1. A function can take zero or more arguments.
+2. In this example, add takes two parameters of type int.
+3. Notice that the type comes after the variable name.
+4. When two or more consecutive named function parameters share a type, you can omit the type from all but the last.
+5. The swap function returns two strings.
+6. A return statement without arguments returns the named return values. This is known as a "naked" return.
+*/
--- ATour/a7functions/stringfunctions.go
+++ ATour/a7functions/stringfunctions.go
@@ -3,3 +3,12 @@ package main
 func swap(x, y string) (string, string) {
 	return y, x
 }
+
+/*
+Notes:
+
+1. Go's return values may be named. If so, they are treated as variables defined at the top of the function.
+2. These names should be used to document the meaning of the return values.
+3. A return statement without arguments returns the current values of the results. This is known as a "naked" return.
+4. Naked return statements should be used only in short functions, as with the example shown here. They can harm readability in longer functions.
+*/
