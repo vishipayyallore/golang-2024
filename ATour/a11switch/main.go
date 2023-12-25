22ea640f8a3783c42a79c4bf961d5b5c014931fc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-25 22:32:39 +0530

    Update main.go

--- ATour/a11switch/main.go
+++ ATour/a11switch/main.go
@@ -46,4 +46,21 @@ func main() {
 	whatAmI(true)
 	whatAmI(1)
 	whatAmI("hey")
+	whatAmI(12.49)
+	whatAmI(12.49 + 1i)
+	whatAmI('a')
+	whatAmI(1 << 2)
+	whatAmI([]int{2, 3})
 }
+
+/*
+Notes:
+
+1. A switch statement is a shorter way to write a sequence of if - else statements. It runs the first case whose value is equal to the condition expression.
+2. Go only runs the selected case, not all the cases that follow. In effect, the break statement that is needed at the end of each case in those languages is provided automatically in Go.
+3. Switch cases evaluate cases from top to bottom, stopping when a case succeeds.
+4. Switch without a condition is the same as switch true.
+5. This construct can be a clean way to write long if-then-else chains.
+6. Type switches compare types instead of values. You can use this to discover the type of an interface value. In this example, the variable t will have the type corresponding to its clause.
+7. You can use commas to separate multiple expressions in the same case statement. We use the optional default case in this example as well.
+*/
