6b273695a21f118ef83672b9222b80d4ccdc1f22
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-02 23:06:28 +0530

    Update main.go

--- gofundamentals/a4pointers/main.go
+++ gofundamentals/a4pointers/main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"autilities"
 	"fmt"
+
+	"github.com/fatih/color"
 )
 
+var header = autilities.Header{}
+
+const headerChar = '*'
+const headerLength = 100
+const headerColor = color.FgHiYellow
+const titleColor = color.FgHiGreen
+
 func main() {
+	header.DisplayHeader(headerChar, "Showing Pointers", headerLength, headerColor, titleColor)
+
 	s := "Hello, world!"
 	autilities.PrintValueAndType(s)
 	p := &s
