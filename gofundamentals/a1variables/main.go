265212a71e604fe1e002f254d3f0ef2fb70a0809
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-02 22:22:15 +0530

    Update main.go

--- gofundamentals/a1variables/main.go
+++ gofundamentals/a1variables/main.go
@@ -6,17 +6,26 @@ func main() {
 	var a string
 	a = "foo"
 
-	fmt.Println(a)
+	// fmt.Println(a)
+	FmtPln(a)
 
 	var b int = 99
-	fmt.Println(b)
+	// fmt.Println(b)
+	FmtPln(b)
 
 	var c = true
-	fmt.Println(c)
+	// fmt.Println(c)
+	FmtPln(c)
 
 	d := 3.1415
-	fmt.Println(d)
+	// fmt.Println(d)
+	FmtPln(d)
 
 	var e int = int(d)
-	fmt.Println(e)
+	// fmt.Println(e)
+	FmtPln(e)
+}
+
+func FmtPln(a ...interface{}) (n int, err error) {
+	return fmt.Println(a...)
 }
