71a88a75f26fe1035abfc567b8b0ff43995cb6cf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-09 16:15:18 +0530

    Update main.go

--- ATour/a8constants/main.go
+++ ATour/a8constants/main.go
@@ -1,23 +1,30 @@
 package main
 
 import (
-	"fmt"
+	utl "autilities"
 	"math"
 )
 
+var header = utl.Header{}
+
+const isManager rune = 'Y'
 const s string = "constant"
 
 func main() {
-	fmt.Println(s)
+	header.DisplayHeader("Constants Demo")
+
+	utl.PLine("String Constant: ", s)
+
+	utl.PLine("Is Manager (rune Constant): ", isManager)
 
 	const n = 500000000
 
 	const d = 3e20 / n
-	fmt.Println(d)
+	utl.PLine("Float Constant: ", d)
 
-	fmt.Println(int64(d))
+	utl.PLine("Int 64 Constant: ", int64(d))
 
-	fmt.Println(math.Sin(n))
+	utl.PLine("Float 64: ", math.Sin(n))
 }
 
 /*
