46cd2a71cd5a41b5438f15f042616b8822c22302
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-21 15:40:28 +0530

    Initial Version

---
+++ learnwebservices/a7headers/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	addr := ":8080"
+	s := http.Server{
+		Addr: addr,
+	}
+	wMessage := "Welcome to Customer Service Web Service.\n"
+	wMessage1 := "Welcome to Customer Service Web API.\n"
+
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, wMessage)
+	}
+
+	// http://localhost:8080
+	http.HandleFunc("/", helloHandler)
+
+	// http://localhost:8080/api
+	http.HandleFunc("/api", helloHandler)
+
+	var getUrlHandlerFunc http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, r.URL.String())
+	}
+
+	// http://localhost:8080/url/ or http://localhost:8080/url/something or http://localhost:8080/url/something/else
+	http.HandleFunc("/url/", getUrlHandlerFunc)
+
+	// http://localhost:8080/handler
+	http.Handle("/handler", handlerInterface(wMessage1))
+
+	fmt.Printf("Starting Web Server at http://localhost%s\n", addr)
+
+	go func() {
+		log.Fatal(s.ListenAndServe())
+	}()
+
+	fmt.Println("Server started, press <Enter> to shutdown")
+	fmt.Scanln()
+	s.Shutdown(context.Background())
+	fmt.Println("Server stopped")
+}
+
+type handlerInterface string
+
+func (h handlerInterface) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, string(h))
+}
