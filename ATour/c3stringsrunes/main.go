17b87723afe42247ae25324b55a0570740cd3002
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-02 09:19:33 +0530

    Update main.go

--- ATour/c3stringsrunes/main.go
+++ ATour/c3stringsrunes/main.go
@@ -8,39 +8,6 @@ var header = utl.Header{}
 
 func main() {
 
-	header.DisplayHeader("Showing Pointers")
-
-	i := 1
-	utl.PFmted("Initial value of i inside main() = %v\n", i)
-
-	utl.PFmted("\nBefore passByValue, i inside main() = %v\n", i)
-	passByValue(i)
-	utl.PFmted("After passByValue, i inside main() = %v\n", i)
-
-	utl.PFmted("\nBefore passByReference Pointer Value, i inside main() = %v\n", &i)
-	utl.PFmted("Before passByReference, i inside main() = %v\n", i)
-	passByReference(&i)
-	utl.PFmted("After passByReference Pointer Value, i inside main() = %v\n", &i)
-	utl.PFmted("After passByReference, i inside main() = %v\n", i)
+	header.DisplayHeader("Showing Strings and Runes")
 
 }
-
-func passByValue(ival int) {
-	utl.PFmted("In passByValue (Before), ival = %v\n", ival)
-	ival = 101
-	utl.PFmted("In passByValue (After), ival = %v\n", ival)
-}
-
-func passByReference(iptr *int) {
-	utl.PFmted("In passByReference (Before), *iptr = %v\n", *iptr)
-	*iptr = 101
-	utl.PFmted("In passByReference (After), *iptr = %v\n", *iptr)
-}
-
-/*
-Notes:
-
-1. We'll show how pointers work in contrast to values with 2 functions: passByValue and passByReference.
-2. We'll also show how to pass a pointer to a function.
-
-*/
