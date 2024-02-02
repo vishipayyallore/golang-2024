d2882d8cfa05535293f513ae48f7410373222bb7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-02 12:36:55 +0530

    Update main.go

--- ATour/c3stringsrunes/main.go
+++ ATour/c3stringsrunes/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	utl "autilities"
+	"unicode/utf8"
 )
 
 var header = utl.Header{}
@@ -10,4 +11,36 @@ func main() {
 
 	header.DisplayHeader("Showing Strings and Runes")
 
+	// TODO: Convert strings into array of strings and show the details of each string
+	// String literals are UTF-8 encoded text
+	s := "สวัสดี"
+	showStringDetails(s)
+	utl.PLine("\n")
+
+	s = "��=� ⌘"
+	showStringDetails(s)
+	utl.PLine("\n")
+
+	s = "世界"
+	showStringDetails(s)
+	utl.PLine("\n")
+
+	s = "Hello"
+	showStringDetails(s)
+}
+
+func showStringDetails(s string) {
+	utl.PFmted("String Value = %v\n", s)
+
+	// A string is a read-only slice of bytes ([]byte).
+	// The length of a string is the number of bytes it contains.
+	utl.PFmted("len(s) i.e number of bytes = %v\n", len(s))
+
+	utl.PLine("Bytes in Hex")
+	for i := 0; i < len(s); i++ {
+		utl.PFmted("%x ", s[i])
+	}
+	utl.PFmted("")
+
+	utl.PFmted("\nRune count: %d", utf8.RuneCountInString(s))
 }
