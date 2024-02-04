81437b4fb2b67ff1e4cf81742cfe055f6e9e5714
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-04 09:06:17 +0530

    Update FileHandlers.go

--- learnwebservices/b1staticcontentfprint/handlers/FileHandlers.go
+++ learnwebservices/b1staticcontentfprint/handlers/FileHandlers.go
@@ -13,8 +13,8 @@ const (
 	CustomersFilePath = "../data/customers.csv"
 )
 
-// handleError responds with an HTTP 500 Internal Server Error and logs the error.
-func handleError(w http.ResponseWriter, err error) {
+// HandleError responds with an HTTP 500 Internal Server Error and logs the error.
+func HandleError(w http.ResponseWriter, err error) {
 	fmt.Println("Error: ", err)
 	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 }
@@ -22,7 +22,7 @@ func handleError(w http.ResponseWriter, err error) {
 func GetCustomerDataHandlerv1(w http.ResponseWriter, req *http.Request) {
 	customerFile, err := os.Open(CustomersFilePath)
 	if err != nil {
-		handleError(w, err)
+		HandleError(w, err)
 		return
 	}
 	defer customerFile.Close()
@@ -30,7 +30,7 @@ func GetCustomerDataHandlerv1(w http.ResponseWriter, req *http.Request) {
 	// ***** Method 1 *****
 	data, err := io.ReadAll(customerFile)
 	if err != nil {
-		handleError(w, err)
+		HandleError(w, err)
 		return
 	}
 
@@ -40,7 +40,7 @@ func GetCustomerDataHandlerv1(w http.ResponseWriter, req *http.Request) {
 func GetCustomerDataHandlerv2(w http.ResponseWriter, req *http.Request) {
 	customerFile, err := os.Open(CustomersFilePath)
 	if err != nil {
-		handleError(w, err)
+		HandleError(w, err)
 		return
 	}
 	defer customerFile.Close()
