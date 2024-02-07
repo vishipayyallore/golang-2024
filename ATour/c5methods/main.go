a4d9f278baeb42b5ce9818875d5ad4c6533d1ec4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-07 15:44:33 +0530

    Update main.go

--- ATour/c5methods/main.go
+++ ATour/c5methods/main.go
@@ -11,16 +11,6 @@ type rect struct {
 	width, height int
 }
 
-func main() {
-	header.DisplayHeader("Showing Methods")
-
-	r := rect{width: 10, height: 5}
-	showValueReceiver(r)
-
-	rp := &r
-	showPointerReceiver(rp)
-}
-
 // Methods can be defined for either pointer or value receiver types. Here’s an example of a value receiver.
 func (r rect) perim() int {
 	return 2*r.width + 2*r.height
@@ -31,14 +21,24 @@ func (r *rect) area() int {
 	return r.width * r.height
 }
 
+func main() {
+	header.DisplayHeader("Showing Methods")
+
+	r := rect{width: 10, height: 5}
+	showValueReceiver(r)
+
+	rp := &r
+	showPointerReceiver(rp)
+}
+
 func showValueReceiver(r rect) {
-	utl.PLine("Rectangle: ", r)
+	utl.PLine("Value Receiver -> Rectangle: ", r)
 	utl.PLine("Area: ", r.area())
 	utl.PLine("Perimeter: ", r.perim())
 }
 
 func showPointerReceiver(rp *rect) {
-	utl.PLine("\nRectangle: ", rp)
+	utl.PLine("\nPointer Receiver -> Rectangle: ", rp)
 	utl.PLine("Area: ", rp.area())
 	utl.PLine("Perimeter: ", rp.perim())
 }
