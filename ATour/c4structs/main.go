ea133d9f459149834bdca24834275f4710bf591e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-07 12:09:38 +0530

    Swamy/07feb work (#49)
    * Update main.go
    
    * Update main.go

--- ATour/c4structs/main.go
+++ ATour/c4structs/main.go
@@ -6,6 +6,11 @@ import (
 
 var header = utl.Header{}
 
+type person struct {
+	name string
+	age  int
+}
+
 func main() {
 
 	header.DisplayHeader("Showing Structs")
@@ -13,11 +18,10 @@ func main() {
 	createPersonStruct()
 
 	constructNewPerson()
-}
 
-type person struct {
-	name string
-	age  int
+	accessStructFields()
+
+	anonymousStruct()
 }
 
 func newPerson(name string) *person {
@@ -27,16 +31,56 @@ func newPerson(name string) *person {
 	return &p
 }
 
+// This is a function to create new person structs. It returns a pointer to the newly created struct.
 func constructNewPerson() {
 	utl.PLine("\nCreating a new person")
+
+	// It’s idiomatic to encapsulate new struct creation in constructor functions.
 	p := newPerson("John")
 	utl.PLine("Person: ", p)
 }
 
 func createPersonStruct() {
-	utl.PLine("Creating a new person struct")
+	utl.PLine("\nCreating a new person struct")
+
+	// This creates a new struct.
 	utl.PLine(person{"Fred", 20})
+
+	// You can name the fields when initializing a struct.
 	utl.PLine(person{name: "Ann", age: 40})
+
+	// Omitted fields will be zero-valued.
 	utl.PLine(person{name: "Alice"})
+
+	// An & prefix yields a pointer to the struct.
 	utl.PLine(&person{name: "Bob", age: 50})
+}
+
+func accessStructFields() {
+	utl.PLine("\nAccessing struct fields")
+
+	s := person{name: "Sean", age: 50}
+	utl.PLine(s.name)
+
+	// You can also use dots with struct pointers - the pointers are automatically dereferenced.
+	sp := &s
+	utl.PLine(sp.age)
+
+	// Structs are mutable.
+	sp.age = 51
+	utl.PLine(sp.age)
+}
+
+// If a struct type is only used for a single value, we don’t have to give it a name.
+// The value can have an anonymous struct type.
+func anonymousStruct() {
+	utl.PLine("\nAnonymous struct")
+
+	// This creates a new struct.
+	a := struct {
+		name string
+		age  int
+	}{"John", 20}
+
+	utl.PLine(a)
 }
