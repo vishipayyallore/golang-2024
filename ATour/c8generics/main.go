eb9cba022a25d7add5b20277f29a7d3af7102753
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-11 17:46:51 +0530

    Update main.go

--- ATour/c8generics/main.go
+++ ATour/c8generics/main.go
@@ -12,6 +12,12 @@ func main() {
 
 }
 
+/*
+ * As an example of a generic function, MapKeys takes a map of any type and returns a slice of its keys.
+ * This function has two type parameters - K and V; K has the comparable constraint, meaning that we can compare values
+ * of this type with the == and != operators. This is required for map keys in Go. V has the any constraint,
+ * meaning that it’s not restricted in any way (any is an alias for interface{}).
+ */
 func MapKeys[K comparable, V any](m map[K]V) []K {
 	r := make([]K, 0, len(m))
 	for k := range m {
@@ -19,4 +25,31 @@ func MapKeys[K comparable, V any](m map[K]V) []K {
 	}
 
 	return r
+}
+
+type element[T any] struct {
+	next *element[T]
+	val  T
+}
+
+// As an example of a generic type, List is a singly-linked list with values of any type.
+type List[T any] struct {
+	head, tail *element[T]
+}
+
+func (lst *List[T]) Push(v T) {
+	if lst.tail == nil {
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
+func (lst *List[T]) GetAll() []T {
+	var elems []T
+	for e := lst.head; e != nil; e = e.next {
+		elems = append(elems, e.val)
+	}
+	return elems
 }
