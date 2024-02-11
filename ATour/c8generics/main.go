af6bafca9680a4e77c90dfe02a551ae755a0f59e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-11 19:59:52 +0530

    Swamy/11feb work (#56)
    * Update main.go
    
    * Update main.go
    
    * Update main.go
    
    * Update main.go

--- ATour/c8generics/main.go
+++ ATour/c8generics/main.go
@@ -7,7 +7,61 @@ import (
 var header = utl.Header{}
 
 func main() {
-	// Starting with version 1.18, Go has added support for generics, also known as type parameters.
+	// Generics, also known as type parameters.
 	header.DisplayHeader("Showing Generics")
 
+	var m = map[int]string{1: "2", 2: "4", 4: "8"}
+	utl.PLine("Map = ", m)
+	utl.PLine("MapKeys(m) = ", MapKeys(m))
+
+	_ = MapKeys[int, string](m)
+	lst := List[int]{}
+	lst.Push(10)
+	lst.Push(13)
+	lst.Push(23)
+
+	utl.PLine("lst.GetAll() = ", lst.GetAll())
+}
+
+/*
+ * As an example of a generic function, MapKeys takes a map of any type and returns a slice of its keys.
+ * This function has two type parameters - K and V; K has the comparable constraint, meaning that we can compare values
+ * of this type with the == and != operators. This is required for map keys in Go. V has the any constraint,
+ * meaning that it’s not restricted in any way (any is an alias for interface{}).
+ */
+func MapKeys[K comparable, V any](m map[K]V) []K {
+	r := make([]K, 0, len(m))
+	for k := range m {
+		r = append(r, k)
+	}
+
+	return r
+}
+
+type element[T any] struct {
+	next *element[T]
+	val  T
+}
+
+// As an example of a generic type, List is a singly-linked list with values of any type.
+type List[T any] struct {
+	head, tail *element[T]
+}
+
+func (lst *List[T]) Push(v T) {
+	if lst.tail == nil {
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
+
+func (lst *List[T]) GetAll() []T {
+	var elems []T
+	for e := lst.head; e != nil; e = e.next {
+		elems = append(elems, e.val)
+	}
+	return elems
 }
