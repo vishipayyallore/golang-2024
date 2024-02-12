2095b8f62d9adea8a4f32cc30119cf78a984c251
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-12 18:11:13 +0530

    Update main.go

--- ATour/d1errors/main.go
+++ ATour/d1errors/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	utl "autilities"
+	"errors"
 )
 
 var header = utl.Header{}
@@ -10,4 +11,20 @@ func main() {
 
 	header.DisplayHeader("Showing Error")
 
+	for _, i := range []int{15, 16, 17, 18, 19, 20} {
+		if r, e := verifyAge(i); e != nil {
+			utl.PLine("Please try after 18 years: ", e)
+		} else {
+			utl.PLine("You are selected: ", r)
+		}
+	}
+}
+
+func verifyAge(age int) (int, error) {
+	if age < 18 {
+		// return fmt.Errorf("Age is less than 18")
+		return -1, errors.New("can't work with under age 18 person")
+	}
+
+	return age, nil
 }
