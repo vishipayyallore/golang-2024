167b6b347699f3ec17803bf24409c0dbefc00509
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-13 13:33:33 +0530

    Update main.go

--- ATour/d2goroutines/main.go
+++ ATour/d2goroutines/main.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	utl "autilities"
+	"time"
 )
 
 var header = utl.Header{}
 
 func main() {
+	// A goroutine is a lightweight thread of execution.
 	header.DisplayHeader("Showing Goroutines")
 
+	f("direct")
+
+	// To invoke this function in a goroutine, use go f(s). This new goroutine will execute concurrently with the calling one.
+	go f("goroutine")
+
+	// You can also start a goroutine for an anonymous function call.
+	go func(msg string) {
+		utl.PLine(msg)
+	}("going")
+
+	time.Sleep(time.Second)
+
+	utl.PLine("done")
+}
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		utl.PLine(from, ":", i)
+	}
 }
