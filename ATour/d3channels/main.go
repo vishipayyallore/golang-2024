128cfe19d7f0f5d62c43e793783441b49e6e680c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-14 10:34:40 +0530

    Update main.go

--- ATour/d3channels/main.go
+++ ATour/d3channels/main.go
@@ -2,32 +2,13 @@ package main
 
 import (
 	utl "autilities"
-	"time"
 )
 
 var header = utl.Header{}
 
 func main() {
-	// A goroutine is a lightweight thread of execution.
-	header.DisplayHeader("Showing Goroutines")
+	// Channels are the pipes that connect concurrent goroutines. You can send values into channels from one goroutine and
+	// receive those values into another goroutine.
+	header.DisplayHeader("Showing Channels")
 
-	f("direct")
-
-	// To invoke this function in a goroutine, use go f(s). This new goroutine will execute concurrently with the calling one.
-	go f("goroutine")
-
-	// You can also start a goroutine for an anonymous function call.
-	go func(msg string) {
-		utl.PLine(msg)
-	}("going")
-
-	time.Sleep(time.Second)
-
-	utl.PLine("done")
-}
-
-func f(from string) {
-	for i := 0; i < 3; i++ {
-		utl.PLine(from, ":", i)
-	}
 }
