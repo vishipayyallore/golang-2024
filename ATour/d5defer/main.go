f7dd49ae761c6a633700b251831b5fd2fadc3552
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-16 15:21:16 +0530

    Update main.go

--- ATour/d5defer/main.go
+++ ATour/d5defer/main.go
@@ -7,7 +7,23 @@ import (
 var header = utl.Header{}
 
 func main() {
-	// to show the use of defer
+	// A defer statement defers the execution of a function until the surrounding function returns.
 	header.DisplayHeader("Showing Defer")
 
+	// The deferred call's arguments are evaluated immediately,
+	// but the function call is not executed until the surrounding function returns.
+	defer utl.PLine("world")
+
+	utl.PLine("hello")
+
+	showDefer()
+}
+
+func showDefer() {
+	// A defer statement defers the execution of a function until the surrounding function returns.
+	// The deferred call's arguments are evaluated immediately,
+	// but the function call is not executed until the surrounding function returns.
+	defer utl.PLine("showDefer() world !!")
+
+	utl.PLine("showDefer() hello ")
 }
