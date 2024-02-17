c5272e2b508c0fb90782f83fd7c32c0396ebb515
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-17 15:10:40 +0530

    Update main.go

--- ATour/d8channeldirections/main.go
+++ ATour/d8channeldirections/main.go
@@ -7,7 +7,19 @@ import (
 var header = utl.Header{}
 
 func main() {
-	//
+	// When using channels as function parameters, you can specify if a channel is meant to only send or receive values.
+	// This specificity increases the type-safety of the program.
 	header.DisplayHeader("Showing Channel Directions")
 
+}
+
+// This ping function only accepts a channel for sending values. It would be a compile-time error to try to receive on this channel.
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
+
+// The pong function accepts one channel for receives (pings) and a second for sends (pongs).
+func pong(pings <-chan string, pongs chan<- string) {
+	msg := <-pings
+	pongs <- msg
 }
