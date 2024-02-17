b0705c7325b23988218032dd11e5ff397edbfe71
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-17 15:02:54 +0530

    Stacking defers

---
+++ ATour/d7stackingdefers/go.mod
@@ -0,0 +1,3 @@
+module d7stackingdefers
+
+go 1.22.0
---
+++ ATour/d7stackingdefers/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	utl "autilities"
+)
+
+var header = utl.Header{}
+
+func main() {
+	//
+	header.DisplayHeader("Showing Stacking defers")
+
+	stackingDefers()
+
+	showDeferedNumbers()
+
+	defer showDeferedNumbersV2()
+
+	utl.PLine("main() done !!")
+}
+
+// Deferred function calls are pushed onto a stack. When a function returns, its deferred calls are executed in last-in-first-out order.
+func stackingDefers() {
+	defer utl.PLine("First")
+	defer utl.PLine("Second")
+	defer utl.PLine("Third")
+}
+
+func showDeferedNumbers() {
+	utl.PLine("Showing Defered Numbers")
+
+	for i := 0; i < 10; i++ {
+		defer utl.PLine(i)
+	}
+
+	utl.PLine("Completed Defered Numbers")
+}
+
+func showDeferedNumbersV2() {
+	utl.PLine("Showing Defered Numbers V2")
+
+	for i := 0; i < 10; i++ {
+		utl.PLine(i)
+	}
+
+	utl.PLine("Completed Defered Numbers V2")
+}
