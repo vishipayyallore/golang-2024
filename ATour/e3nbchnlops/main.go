bb5e6065ed33fde88cdea5d3cefc4082e7e23be4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-22 15:09:07 +0530

    Update main.go

--- ATour/e3nbchnlops/main.go
+++ ATour/e3nbchnlops/main.go
@@ -7,5 +7,38 @@ import (
 var header = utl.Header{}
 
 func main() {
+	// Basic sends and receives on channels are blocking. However, we can use select with a default clause to implement non-blocking sends,
+	// receives, and even non-blocking multi-way selects.
 	header.DisplayHeader("Showing Non-Blocking Channel Operations")
+
+	showDemo1()
+}
+
+func showDemo1() {
+	messages := make(chan string)
+	signals := make(chan bool)
+
+	select {
+	case msg := <-messages:
+		utl.PLine("received message", msg)
+	default:
+		utl.PLine("no message received")
+	}
+
+	msg := "hi"
+	select {
+	case messages <- msg:
+		utl.PLine("sent message", msg)
+	default:
+		utl.PLine("no message sent")
+	}
+
+	select {
+	case msg := <-messages:
+		utl.PLine("received message", msg)
+	case sig := <-signals:
+		utl.PLine("received signal", sig)
+	default:
+		utl.PLine("no activity")
+	}
 }
