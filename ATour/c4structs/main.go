792b838966159c12e0317e751587c600e80fef00
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-24 13:50:33 +0530

    Update main.go

--- ATour/c4structs/main.go
+++ ATour/c4structs/main.go
@@ -26,6 +26,8 @@ func main() {
 
 	showPointerToStruct()
 
+	showStructLiterals()
+
 	showVertexDemo()
 
 	createPersonStruct()
@@ -35,6 +37,23 @@ func main() {
 	accessStructFields()
 
 	anonymousStruct()
+}
+
+// Struct Literals
+func showStructLiterals() {
+	utl.PLine("\nStruct Literals")
+
+	// A struct literal denotes a newly allocated struct value by listing the values of its fields.
+	v1 := Vertex{1, 2}
+
+	// You can list just a subset of fields by using the Name: syntax. (And the order of named fields is irrelevant.)
+	v2 := Vertex{X: 1} // Y:0 is implicit
+	v3 := Vertex{}     // X:0 and Y:0
+
+	// The special prefix & returns a pointer to the struct value.
+	p := &Vertex{1, 2}
+
+	utl.PLine(v1, p, v2, v3)
 }
 
 // Pointers to structs
