42335503dbfc7d3b7c414e61cf0e622be2c63c59
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-25 11:46:46 +0530

    Swamy/25feb work (#79)
    * Update main.go
    
    * Update README.md
    
    * INitial Version
    
    * Update main.go
    
    * Update main.go
    
    * Update main.go
    
    * Changes
    
    * Changes

--- learnwebservices/d1basicrouting/handlers/ProductsHandler.go
+++ learnwebservices/d1basicrouting/handlers/ProductsHandler.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 // GET http://localhost:8081/api/products
@@ -28,7 +29,6 @@ func GetAllProductByQueryStringHandler(w http.ResponseWriter, r *http.Request) {
 	idRaw := r.URL.Query().Get("id")
 
 	id, err := strconv.Atoi(idRaw)
-
 	if err != nil {
 		log.Print(err)
 		w.WriteHeader(http.StatusBadRequest)
@@ -40,6 +40,39 @@ func GetAllProductByQueryStringHandler(w http.ResponseWriter, r *http.Request) {
 			data, err := json.Marshal(p)
 			if err != nil {
 				log.Print(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			w.Header().Add("Content-Type", "application/json")
+			w.Write(data)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+}
+
+// GET http://localhost:8081/api/products/1
+func GetAllProductByRouteParameterHandler(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.Path, "/")
+	log.Print(" URL: ", r.URL.Path, " | Parts : ", parts, " | Length: ", len(parts))
+
+	if len(parts) != 4 { // path: /products/1 -> [ "" "products" "1"]
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	id, err := strconv.Atoi(parts[3])
+	if err != nil {
+		log.Print("Error: ", err, " Parts: ", parts)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	for _, p := range ent.Products {
+		if id == p.ID {
+			data, err := json.Marshal(p)
+			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
