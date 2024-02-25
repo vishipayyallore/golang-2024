a67d7741511a36188e226a9b61af010d42cf3769
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-25 11:09:14 +0530

    Update main.go

--- ATour/e6timers/main.go
+++ ATour/e6timers/main.go
@@ -7,7 +7,8 @@ import (
 var header = utl.Header{}
 
 func main() {
-	//
+	// We often want to execute Go code at some point in the future, or repeatedly at some interval.
+	// Go’s built-in timer and ticker features make both of these tasks easy.
 	header.DisplayHeader("Showing Timers")
 
 }
