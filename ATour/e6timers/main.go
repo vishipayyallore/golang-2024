24a3c121d5a1eac7cb20bd7b4a6569693f43b59e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-25 11:18:22 +0530

    Update main.go

--- ATour/e6timers/main.go
+++ ATour/e6timers/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	utl "autilities"
+	"time"
 )
 
 var header = utl.Header{}
@@ -11,4 +12,11 @@ func main() {
 	// Go’s built-in timer and ticker features make both of these tasks easy.
 	header.DisplayHeader("Showing Timers")
 
+	// Timers represent a single event in the future. You tell the timer how long you want to wait, and
+	// it provides a channel that will be notified at that time. This timer will wait 2 seconds.
+	timer1 := time.NewTimer(2 * time.Second)
+
+	// The <-timer1.C blocks on the timer’s channel C until it sends a value indicating that the timer fired.
+	<-timer1.C
+	utl.PLine("Timer 1 fired")
 }
