70fdf8ec987381490cdc33b34cd8ef1156c6064d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-26 13:44:18 +0530

    Update main.go

--- ATour/e7tickers/main.go
+++ ATour/e7tickers/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	utl "autilities"
+	"time"
 )
 
 var header = utl.Header{}
@@ -11,4 +12,26 @@ func main() {
 	// Tickers are for when you want to do something repeatedly at regular intervals.
 	header.DisplayHeader("Showing Tickers")
 
+	// Tickers use a similar mechanism to timers: a channel that is sent values.
+	ticker := time.NewTicker(500 * time.Millisecond)
+	done := make(chan bool)
+
+	// Here we’ll use the select builtin on the channel to await the values as they arrive every 500ms.
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				utl.PLine("Tick at", t)
+			}
+		}
+	}()
+
+	time.Sleep(1600 * time.Millisecond)
+	ticker.Stop()
+
+	done <- true
+
+	utl.PLine("Ticker stopped")
 }
