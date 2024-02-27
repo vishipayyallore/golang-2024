feab7651b3064b360d28127f6fc6a4d1bdefcc17
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-27 08:09:30 +0530

    Update main.go

--- ATour/b5slices/main.go
+++ ATour/b5slices/main.go
@@ -117,7 +117,7 @@ func showCliceDemo() {
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 
 	// A slice is formed by specifying two indices, a low and high bound, separated by a colon:
-	// a[low : high]
+	// a[low : high] = This selects a half-open range which includes the first element, but excludes the last one.
 	var s []int = primes[1:4]
 	utl.PLine("Slice: ", s)
 }
