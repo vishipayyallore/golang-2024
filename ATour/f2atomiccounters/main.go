45b8f6bb45d6bc6192c624f3b2504edff32674c4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-03 22:19:54 +0530

    Update main.go

--- ATour/f2atomiccounters/main.go
+++ ATour/f2atomiccounters/main.go
@@ -7,7 +7,11 @@ import (
 var header = utl.Header{}
 
 func main() {
-	//
+	/*
+		The primary mechanism for managing state in Go is communication over channels. We saw this for example with worker pools.
+		There are a few other options for managing state though. Here we’ll look at using the sync/atomic package for atomic counters
+		accessed by multiple goroutines.
+	*/
 	header.DisplayHeader("Showing Atomic Counters")
 
 }
