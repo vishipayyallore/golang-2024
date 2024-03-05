f38fbde5625fbb63b2e94654208393518c59dc79
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-05 14:41:01 +0530

    Stateful Goroutines Initial Version

---
+++ ATour/f4statefulgoroutines/go.mod
@@ -0,0 +1,3 @@
+module f4statefulgoroutines
+
+go 1.22.0
---
+++ ATour/f4statefulgoroutines/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	utl "autilities"
+)
+
+var header = utl.Header{}
+
+func main() {
+	/*
+
+	 */
+	header.DisplayHeader("Showing Stateful Goroutines")
+
+}
