9a649abd0ccb0ef2a2437eefba697c6e0b94af39
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-05 14:43:08 +0530

    Update main.go

--- ATour/f4statefulgoroutines/main.go
+++ ATour/f4statefulgoroutines/main.go
@@ -8,8 +8,10 @@ var header = utl.Header{}
 
 func main() {
 	/*
-
-	 */
+		We used explicit locking with mutexes to synchronize access to shared state across multiple goroutines. Another option is to use
+		the built-in synchronization features of goroutines and channels to achieve the same result. This channel-based approach aligns
+		with Go’s ideas of sharing memory by communicating and having each piece of data owned by exactly 1 goroutine.
+	*/
 	header.DisplayHeader("Showing Stateful Goroutines")
 
 }
