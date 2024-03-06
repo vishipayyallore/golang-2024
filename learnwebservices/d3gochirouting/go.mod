98b32a61a5cb4a2cfc44210b50294cc63446e50a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-06 13:18:23 +0530

    Changes

--- learnwebservices/d3gochirouting/go.mod
+++ learnwebservices/d3gochirouting/go.mod
@@ -1,3 +1,5 @@
 module d3gochirouting
 
 go 1.22.1
+
+require github.com/go-chi/chi v1.5.5 // indirect
---
+++ learnwebservices/d3gochirouting/main.go
@@ -0,0 +1,54 @@
+// main.go
+
+package main
+
+import (
+	"context"
+	pHdls "d1basicrouting/handlers"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+
+	r := chi.NewRouter()
+
+	// GET http://localhost:8082
+	r.HandleFunc("/", pHdls.ServerHomeHtml)
+
+	// GET http://localhost:8082/api
+	r.HandleFunc("/api", pHdls.ServerHomeHtml)
+
+	// GET http://localhost:8082/api/products
+	r.HandleFunc("/api/products", pHdls.GetAllProductsHandler)
+
+	// GET http://localhost:8082/api/products/?id=1
+	r.HandleFunc("/api/products/", pHdls.GetAllProductByQueryStringHandler)
+
+	// GET http://localhost:8082/api/products-qs?id=2
+	r.HandleFunc("/api/products-qs", pHdls.GetAllProductByQueryStringHandler)
+
+	// GET http://localhost:8082/api/products-ssplit/3
+	r.HandleFunc("/api/products/{id:[0-9]+}", pHdls.GetAllProductByRouteParameterHandlerGMux)
+
+	http.Handle("/", r)
+
+	addr := ":8082"
+	s := http.Server{
+		Addr: addr,
+	}
+
+	fmt.Printf("Starting Web Server at http://localhost%s\n", addr)
+
+	go func() {
+		log.Fatal(s.ListenAndServe())
+	}()
+
+	fmt.Println("Server started, press <Enter> to shutdown")
+	fmt.Scanln()
+	s.Shutdown(context.Background())
+	fmt.Println("Server stopped")
+}
