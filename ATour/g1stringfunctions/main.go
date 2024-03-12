07a9ca42fa9e499222b5fcecaa01e80806982d23
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-12 14:02:08 +0530

    Update main.go

--- ATour/g1stringfunctions/main.go
+++ ATour/g1stringfunctions/main.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	utl "autilities"
+	s "strings"
 )
 
 var header = utl.Header{}
 
+// We alias fmt.Println to a shorter name as we’ll use it a lot below.
+var p = utl.PLine
+
 /*
- */
+The standard library’s strings package provides many useful string-related functions.
+*/
 func main() {
 	/*
 	 */
 	header.DisplayHeader("Showing String Functions")
 
+	p("Contains:  ", s.Contains("test", "es"))
+	p("Count:     ", s.Count("test", "t"))
+	p("HasPrefix: ", s.HasPrefix("test", "te"))
+	p("HasSuffix: ", s.HasSuffix("test", "st"))
+	p("Index:     ", s.Index("test", "e"))
+	p("Join:      ", s.Join([]string{"a", "b"}, "-"))
+	p("Repeat:    ", s.Repeat("a", 5))
+	p("Replace:   ", s.Replace("foo", "o", "0", -1))
+	p("Replace:   ", s.Replace("foo", "o", "0", 1))
+	p("Split:     ", s.Split("a-b-c-d-e", "-"))
+	p("ToLower:   ", s.ToLower("TEST"))
+	p("ToUpper:   ", s.ToUpper("test"))
 }
