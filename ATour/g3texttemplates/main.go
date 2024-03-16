633bce563ed8078db201cee12ae7275f14b25934
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-16 21:36:48 +0530

    Update main.go

--- ATour/g3texttemplates/main.go
+++ ATour/g3texttemplates/main.go
@@ -40,4 +40,29 @@ func main() {
 		"C#",
 	})
 
+	// If the data is a struct we can use the {{.FieldName}} action to access its fields.
+	// The fields should be exported to be accessible when a template is executing.
+	t2 := Create("t2", "Name: {{.Name}}\n")
+
+	// The same applies to maps; with maps there is no restriction on the case of key names.
+	t2.Execute(os.Stdout, struct{ Name string }{"Jane Doe"})
+
+	t2.Execute(os.Stdout, map[string]string{"Name": "Mickey Mouse"})
+
+	// if/else provide conditional execution for templates. A value is considered false if it’s the default value of a type, such as 0,
+	// an empty string, nil pointer, etc. This sample demonstrates another feature of templates: using - in actions to trim whitespace.
+	t3 := Create("t3", "{{if . -}} yes {{else -}} no {{end}}\n")
+	t3.Execute(os.Stdout, "not empty")
+	t3.Execute(os.Stdout, "")
+
+	// range blocks let us loop through slices, arrays, maps or channels. Inside the range block {{.}} is set to the current
+	// item of the iteration.
+	t4 := Create("t4", "Range: {{range .}}{{.}} {{end}}\n")
+
+	t4.Execute(os.Stdout, []string{"Go", "Rust", "C++", "C#"})
+}
+
+// Create is a function that creates a new template and parses its body from a string.
+var Create = func(name, t string) *template.Template {
+	return template.Must(template.New(name).Parse(t))
 }
