741be88f8f704d7c2b74f87bbe0a33873257efd3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-19 12:57:42 +0530

    Update main.go

--- ATour/g6json/main.go
+++ ATour/g6json/main.go
@@ -9,13 +9,13 @@ import (
 var header = utl.Header{}
 
 // We’ll use these two structs to demonstrate encoding and decoding of custom types below.
-type response1 struct {
+type Response1 struct {
 	Page   int
 	Fruits []string
 }
 
 // Only exported fields will be encoded/decoded in JSON. Fields must start with capital letters to be exported.
-type response2 struct {
+type Response2 struct {
 	Page   int      `json:"page"`
 	Fruits []string `json:"fruits"`
 }
@@ -83,7 +83,7 @@ func showDecodingJson() {
 
 	// We can also decode JSON into custom data types. This has the advantages of adding additional type-safety to our programs and eliminating the need for type assertions when accessing the decoded data.
 	str := `{"page": 1, "fruits": ["apple", "peach"]}`
-	res := response2{}
+	res := Response2{}
 	json.Unmarshal([]byte(str), &res)
 	utl.PLine(res)
 	utl.PLine(res.Fruits[0])
@@ -93,14 +93,14 @@ func showCustomTypesAsJson() {
 	utl.PLine("\nShowing custom types as JSON")
 
 	// The JSON package can automatically encode your custom data types. It will only include exported fields in the encoded output and will by default use those names as the JSON keys.
-	res1D := &response1{
+	res1D := &Response1{
 		Page:   1,
 		Fruits: []string{"apple", "peach", "pear"}}
 	res1B, _ := json.Marshal(res1D)
 	utl.PLine(string(res1B))
 
 	// You can use tags on struct field declarations to customize the encoded JSON key names. Check the definition of response2 above to see an example of such tags.
-	res2D := &response2{
+	res2D := &Response2{
 		Page:   1,
 		Fruits: []string{"apple", "peach", "pear"}}
 	res2B, _ := json.Marshal(res2D)
