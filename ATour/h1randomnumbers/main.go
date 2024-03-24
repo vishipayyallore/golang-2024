613b2a57b52f1463b0c348f4ca985bd42bd72357
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-24 14:02:27 +0530

    Initial Version

---
+++ ATour/h1randomnumbers/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	utl "autilities"
+)
+
+var header = utl.Header{}
+
+/*
+Go’s math/rand/v2 package provides pseudorandom number generation.
+*/
+func main() {
+	/*
+	 */
+	header.DisplayHeader("Showing Random Numbers")
+}
