e55badb0297acdbc9fbfaded40ef620d7ebcb0c1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-31 13:46:07 +0530

    Swamy/31mar work (#131)
    * Changes
    
    * Writing Files Initial Version
    
    * Changes

--- ATour/c5methods/main.go
+++ ATour/c5methods/main.go
@@ -89,6 +89,12 @@ func showValueReceiver(r Rect) {
 	utl.PLine("Perimeter: ", r.perim())
 }
 
+/*
+There are two reasons to use a pointer receiver. The first is so that the method can modify the value that its receiver points to. The second
+is to avoid copying the value on each method call. This can be more efficient if the receiver is a large struct, for example. In this example,
+both Scale and Abs are methods with receiver type *Vertex, even though the Abs method needn't modify its receiver. In general, all methods on
+a given type should have either value or pointer receivers, but not a mixture of both.
+*/
 func showPointerReceiver(rp *Rect) {
 	utl.PLine("\nPointer Receiver -> Rectangle: ", rp)
 	utl.PLine("Area: ", rp.area())
