69991139ef828c97887a8eff5e7d19e39350e3dc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-31 13:45:02 +0530

    Changes

--- ATour/h7writingfiles/main.go
+++ ATour/h7writingfiles/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	utl "autilities"
+	"bufio"
 	"os"
 )
 
 var header = utl.Header{}
 
 const filePath string = "./data/sample.txt"
+const file1Path string = "./data/sample1.txt"
 
 /*
 Writing files in Go follows similar patterns to the ones we saw earlier for reading.
@@ -21,6 +23,36 @@ func main() {
 	d1 := []byte("hello\ngo\n")
 	err := os.WriteFile(filePath, d1, 0644)
 	check(err)
+
+	// For more granular writes, open a file for writing.
+	f, err := os.Create(file1Path)
+	check(err)
+
+	// It’s idiomatic to defer a Close immediately after opening a file.
+	defer f.Close()
+
+	// You can Write byte slices as you’d expect.
+	d2 := []byte{115, 111, 109, 101, 10}
+	n2, err := f.Write(d2)
+	check(err)
+	utl.PFmted("wrote %d bytes\n", n2)
+
+	// A WriteString is also available.
+	n3, err := f.WriteString("writes\n")
+	check(err)
+	utl.PFmted("wrote %d bytes\n", n3)
+
+	// Issue a Sync to flush writes to stable storage.
+	f.Sync()
+
+	// bufio provides buffered writers in addition to the buffered readers we saw earlier.
+	w := bufio.NewWriter(f)
+	n4, err := w.WriteString("buffered\n")
+	check(err)
+	utl.PFmted("wrote %d bytes\n", n4)
+
+	// Use Flush to ensure all buffered operations have been applied to the underlying writer.
+	w.Flush()
 }
 
 func check(e error) {
