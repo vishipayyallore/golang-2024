70872ade81b87e3c6c8bd3ad1c3de3b84d0d25db
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-01 21:42:31 +0530

    Update main.go

--- ATour/h8interfaces/main.go
+++ ATour/h8interfaces/main.go
@@ -7,10 +7,25 @@ import (
 var header = utl.Header{}
 
 /*
- */
+An interface type is defined as a set of method signatures.
+*/
 func main() {
 	/*
 	 */
 	header.DisplayHeader("Showing Interfaces")
 
+}
+
+type Abser interface {
+	Abs() float64
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+
+	return float64(f)
 }
