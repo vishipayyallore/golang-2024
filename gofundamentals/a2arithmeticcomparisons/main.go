c254be46ef1ab20dc597402bab4e2404023840d5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-02 17:48:40 +0530

    Changes

--- gofundamentals/a2arithmeticcomparisons/main.go
+++ gofundamentals/a2arithmeticcomparisons/main.go
@@ -21,8 +21,8 @@ func main() {
 	n1, n2 := 5, 2
 
 	fmt.Println("Addition: ", n1+n2)
-	fmt.Println(n1 - n2)
-	fmt.Println(n1 * n2)
+	fmt.Println("Substraction: ", n1-n2)
+	fmt.Println("Multiplication: ", n1*n2)
 	fmt.Println("Integer Division: ", n1/n2) // Integer Division
 	fmt.Println("Float Division: ", float64(n1)/float64(n2))
 	fmt.Println("Modulus: ", n1%n2) // Modulus
--- gofundamentals/a3constants/main.go
+++ gofundamentals/a3constants/main.go
@@ -17,11 +17,14 @@ func main() {
 
 	header.DisplayHeader(headerChar, "Showing Constants", headerLength, headerColor, titleColor)
 
-	const a = 42
+	const a = 42 // untyped constant OR Implicitly typed constant
 
 	autilities.PrintValueAndType(a)
-	const b float32 = 3
+	const b float32 = 3 // typed constant OR Explicitly typed constant
 	autilities.PrintValueAndType(b)
+
+	const aa = a // One constant can be assigned to another constant
+	autilities.PrintValueAndType(aa)
 
 	var i int = a
 	autilities.PrintValueAndType(i)
@@ -30,15 +33,41 @@ func main() {
 	f32 = b
 	autilities.PrintValueAndType(f32)
 
+	// Constants can be used in expressions
+	const cccc = 2 * 5
+	autilities.PrintValueAndType(cccc)
+	const dddd = "Hello " + "Gophers"
+	autilities.PrintValueAndType(dddd)
+
 	const c = iota
 	autilities.PrintValueAndType(c)
 
+	// Group of Constants
+	const (
+		bb int = 1
+		cc int = 2
+	)
+	autilities.PrintValueAndType(bb)
+	autilities.PrintValueAndType(cc)
+
+	const (
+		aaa string = "Hello"
+		bbb        = "World"
+		ccc        = "!!!"
+		ddd        // Uninitialized constant will take the value of the previous constant
+	)
+	autilities.PrintValueAndType(aaa)
+	autilities.PrintValueAndType(bbb)
+	autilities.PrintValueAndType(ccc)
+	autilities.PrintValueAndType(ddd)
+
+	// Grouping constants
 	const (
 		d = 2 * 5
 		e
-		f = iota
+		f = iota // iota value is 2
 		g
-		h = 10 * iota
+		h = 10 * iota // iota value is 4 as it is in 5th position
 	)
 
 	autilities.PrintValueAndType(d)
@@ -46,4 +75,12 @@ func main() {
 	autilities.PrintValueAndType(f)
 	autilities.PrintValueAndType(g)
 	autilities.PrintValueAndType(h)
+
+	// iota is scoped to the constant block
+	// iota is reset to 0 for each constant block
+	const (
+		a1 = iota
+	)
+	autilities.PrintValueAndType(a1)
+
 }
