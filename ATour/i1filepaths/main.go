4a5133d0e67aca2b88f2e339f18f22e9ae2c603f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-04 12:40:01 +0530

    Initial Versiion

---
+++ ATour/i1filepaths/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	utl "autilities"
+)
+
+var header = utl.Header{}
+
+/*
+The filepath package provides functions to parse and construct file paths in a way that is portable between operating systems; dir/file on Linux vs. dir\file on Windows, for example.
+*/
+func main() {
+	/*
+	 */
+	header.DisplayHeader("Showing File Paths")
+
+}
