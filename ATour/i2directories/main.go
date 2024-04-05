ac8c3fd68941ba29fc680cd6f9be9fe9dcf7e69b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-05 09:23:40 +0530

    Update main.go

--- ATour/i2directories/main.go
+++ ATour/i2directories/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	utl "autilities"
+	"io/fs"
+	"os"
+	"path/filepath"
 )
 
 var header = utl.Header{}
@@ -14,10 +17,68 @@ func main() {
 	 */
 	header.DisplayHeader("Showing Directories")
 
+	// Create a new sub-directory in the current working directory.
+	err := os.Mkdir("subdir", 0755)
+	check(err)
+
+	// When creating temporary directories, it’s good practice to defer their removal. os.RemoveAll will delete a whole directory tree (similarly to rm -rf).
+	defer os.RemoveAll("subdir")
+
+	// Helper function to create a new empty file.
+	createEmptyFile := func(name string) {
+		d := []byte("")
+		check(os.WriteFile(name, d, 0644))
+	}
+	createEmptyFile("subdir/file1")
+
+	// We can create a hierarchy of directories, including parents with MkdirAll. This is similar to the command-line mkdir -p.
+	err = os.MkdirAll("subdir/parent/child", 0755)
+	check(err)
+
+	createEmptyFile("subdir/parent/file2")
+	createEmptyFile("subdir/parent/file3")
+	createEmptyFile("subdir/parent/child/file4")
+
+	// ReadDir lists directory contents, returning a slice of os.DirEntry objects.
+	c, err := os.ReadDir("subdir/parent")
+	check(err)
+	utl.PLine("Listing subdir/parent")
+	for _, entry := range c {
+		utl.PLine(" ", entry.Name(), entry.IsDir())
+	}
+
+	// Chdir lets us change the current working directory, similarly to cd.
+	err = os.Chdir("subdir/parent/child")
+	check(err)
+
+	// Now we’ll see the contents of subdir/parent/child when listing the current directory.
+	c, err = os.ReadDir(".")
+	check(err)
+	utl.PLine("Listing subdir/parent/child")
+	for _, entry := range c {
+		utl.PLine(" ", entry.Name(), entry.IsDir())
+	}
+
+	// cd back to where we started.
+	err = os.Chdir("../../..")
+	check(err)
+
+	// We can also visit a directory recursively, including all its sub-directories. WalkDir accepts a callback function to handle every file or directory visited.
+	utl.PLine("Visiting subdir")
+	err = filepath.WalkDir("subdir", visit)
+	check(err)
 }
 
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func visit(path string, d fs.DirEntry, err error) error {
+	if err != nil {
+		return err
+	}
+	utl.PLine(" ", path, d.IsDir())
+	return nil
 }
