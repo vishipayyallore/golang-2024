456d6cf5675de25dd931d9c76a04df4d41c150b1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-06 08:51:22 +0530

    Update main.go

--- ATour/h8interfacesv1/main.go
+++ ATour/h8interfacesv1/main.go
@@ -16,34 +16,7 @@ func main() {
 	*/
 	header.DisplayHeader("Showing Interfaces")
 
-	var a Abser
-	f := FloatValue(-math.Sqrt2)
-	v := Vertex{3, 4}
-
-	utl.PLine("A: ", a)
-	utl.PLine("F: ", f)
-	utl.PLine("V: ", v)
-
-	utl.PLine("FloatValue(-math.Sqrt2).Abs() = ", f.Abs())
-	utl.PLine("Vertex{3, 4}.Abs() = ", v.Abs())
-
-	a = f // a MyFloat implements Abser
-	utl.PLine("A: ", a)
-	utl.PLine("a.Abs() = ", a.Abs())
-
-	// In the following line, v is a Vertex (not *Vertex) and does NOT implement Abser.
-	// cannot use v (variable of type Vertex) as Abser value in assignment: Vertex does not implement Abser (method Abs has pointer receiver)
-	// a = v
-
-	a = &v // a *Vertex implements Abser
-	utl.PLine("A: ", a)
-	utl.PLine("a.Abs() = ", a.Abs())
-
 	// Implicit interfaces
-	var i I = P{Age: 25, Name: "John"}
-	i.Display()
-	describe(i)
-
 	var i1 I = &P{Age: 25, Name: "John"}
 	i1.Display()
 	describe(i1)
@@ -51,35 +24,6 @@ func main() {
 	var i2 = F(math.Pi)
 	i2.Display()
 	describe(i2)
-}
-
-type Abser interface {
-	Abs() float64
-}
-
-type FloatValue float64
-
-func (f FloatValue) Abs() float64 {
-	if f < 0 {
-		return float64(-f)
-	}
-
-	return float64(f)
-}
-
-type F float64
-
-func (f F) Display() {
-	utl.PLine("\nDisplaying Float F.Display()")
-	utl.PLine(f)
-}
-
-type Vertex struct {
-	X, Y float64
-}
-
-func (v *Vertex) Abs() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 /*
@@ -97,10 +41,22 @@ type P struct {
 
 // This method means type T implements the interface I,
 // but we don't need to explicitly declare that it does so.
-func (p P) Display() {
+func (p *P) Display() {
 	utl.PLine("\nDisplaying Person")
+	if p == nil {
+		utl.PLine("Person is <nil>")
+		return
+	}
+
 	utl.PLine("Age: ", p.Age)
 	utl.PLine("Name: ", p.Name)
+}
+
+type F float64
+
+func (f F) Display() {
+	utl.PLine("\nDisplaying Float F.Display()")
+	utl.PLine(f)
 }
 
 /*
