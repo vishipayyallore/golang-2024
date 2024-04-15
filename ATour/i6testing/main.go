40f2e93daefa4413a855febabee3ec9d76c561cc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-15 12:09:02 +0530

    Initial Version

---
+++ ATour/i6testing/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	utl "autilities"
+)
+
+var header = utl.Header{}
+
+/*
+ */
+func main() {
+	/*
+	 */
+	header.DisplayHeader("Showing Testing and Benchmarking in GoLang")
+
+}
