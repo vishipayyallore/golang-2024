577959320c91af355becac6cccf6b29ce97e4b02
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-16 16:02:47 +0530

    Changes

---
+++ gofundamentals/b3collectionsloops/go.mod
@@ -0,0 +1,3 @@
+module b3collectionsloops
+
+go 1.22.2
---
+++ gofundamentals/b3collectionsloops/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Welcome to the coffee shop!")
+	fmt.Println("\nHere is our menu:")
+
+	fmt.Println("Please select an option")
+	fmt.Println("1) Print menu")
+	in := bufio.NewReader(os.Stdin)
+	choice, _ := in.ReadString('\n')
+	choice = strings.TrimSpace(choice) // we don't know what to do with this yet!
+	fmt.Println("\nYou chose:", choice)
+
+	menu := []menuItem{
+		{name: "Coffee", prices: map[string]float64{"small": 1.65, "medium": 1.80, "large": 1.95}},
+		{name: "Espresso", prices: map[string]float64{"single": 1.90, "double": 2.25, "triple": 2.55}},
+	}
+
+	for _, item := range menu {
+		fmt.Println(item.name)
+		fmt.Println(strings.Repeat("-", 10))
+		for size, cost := range item.prices {
+			fmt.Printf("\t%10s%10.2f\n", size, cost)
+		}
+	}
+
+}
+
+type menuItem struct {
+	name   string
+	prices map[string]float64
+}
