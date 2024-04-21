42cc9e528508168c4d5aa2602f982c133b8c765d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-21 13:11:51 +0530

    Update main.go

--- ATour/i7cmdlineargs/main.go
+++ ATour/i7cmdlineargs/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	utl "autilities"
+	"flag"
 	"fmt"
 	"os"
 )
@@ -11,6 +12,7 @@ var header = utl.Header{}
 /*
 Command-line arguments are a common way to parameterize execution of programs.
 Execution: go run . ARG1 ARG2 ARG3 ...
+Execution: go run . -word=opt -numb=7 -fork -svar=flag ARG1 ARG2 ARG3 ...
 */
 func main() {
 	/*
@@ -30,4 +32,18 @@ func main() {
 		arg := os.Args[3]
 		fmt.Println("Third Argument: ", arg)
 	}
+
+	wordPtr := flag.String("word", "foo", "a string")
+	numbPtr := flag.Int("numb", 42, "an int")
+	forkPtr := flag.Bool("fork", false, "a bool")
+
+	var svar string
+	flag.StringVar(&svar, "svar", "bar", "a string var")
+	flag.Parse()
+
+	fmt.Println("word:", *wordPtr)
+	fmt.Println("numb:", *numbPtr)
+	fmt.Println("fork:", *forkPtr)
+	fmt.Println("svar:", svar)
+	fmt.Println("tail:", flag.Args())
 }
