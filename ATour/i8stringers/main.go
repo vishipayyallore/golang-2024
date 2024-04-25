3ec561aa39b2066858e186402b92a9cf0b5d84ec
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-25 19:04:18 +0530

    Changes

--- ATour/i8stringers/main.go
+++ ATour/i8stringers/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	utl "autilities"
 	"fmt"
+	"strings"
 )
 
 var header = utl.Header{}
@@ -18,6 +19,15 @@ func main() {
 	utl.PLine("A: ", a)
 	z := Person{"Zaphod Beeblebrox", 9001}
 	utl.PLine("Z: ", z)
+
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
 }
 
 type Person struct {
@@ -27,6 +37,23 @@ type Person struct {
 
 func (p Person) String() string {
 	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
+}
+
+type IPAddr [4]byte
+
+// String method for IPAddr to implement fmt.Stringer interface
+func (ip IPAddr) String() string {
+	// Create a slice of strings to hold each byte converted to string
+	strBytes := make([]string, len(ip))
+
+	// Iterate over each byte in the IP address
+	for i, b := range ip {
+		// Convert each byte to string and add to the slice
+		strBytes[i] = fmt.Sprintf("%d", b)
+	}
+
+	// Join the slice of strings with "." to form the dotted quad
+	return strings.Join(strBytes, ".")
 }
 
 /*
