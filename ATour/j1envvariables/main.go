d1e07a8b7daa0112fc06094b0735b9d8a3ace6b6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-25 19:08:43 +0530

    Update main.go

--- ATour/j1envvariables/main.go
+++ ATour/j1envvariables/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	utl "autilities"
+	"os"
+	"strings"
 )
 
 var header = utl.Header{}
@@ -15,4 +17,15 @@ func main() {
 	 */
 	header.DisplayHeader("Showing Environment Variables")
 
+	// To set a key/value pair, use os.Setenv. To get a value for a key, use os.Getenv. This will return an empty string if the key isn’t present in the environment.
+	os.Setenv("FOO", "1")
+	utl.PLine("FOO:", os.Getenv("FOO"))
+	utl.PLine("BAR:", os.Getenv("BAR"))
+
+	// Use os.Environ to list all key/value pairs in the environment. This returns a slice of strings in the form KEY=value. You can strings.SplitN them to get the key and value. Here we print all the keys.
+	utl.PLine()
+	for _, e := range os.Environ() {
+		pair := strings.SplitN(e, "=", 2)
+		utl.PLine(pair[0])
+	}
 }
