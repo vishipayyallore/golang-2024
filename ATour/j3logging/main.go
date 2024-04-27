dfa4bc412578f63ec68ffbf6beecefbf5f231f22
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-27 16:41:15 +0530

    Initial Version

---
+++ ATour/j3logging/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	utl "autilities"
+)
+
+var header = utl.Header{}
+
+/*
+The Go standard library provides straightforward tools for outputting logs from Go programs, with the log package for
+free-form output and the log/slog package for structured output.
+*/
+func main() {
+	/*
+	 */
+	header.DisplayHeader("Showing Logging")
+
+}
