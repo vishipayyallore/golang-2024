b2a68868f932a25edcc7c979c86466c02cb7bfa7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-27 16:44:35 +0530

    Update main.go

--- ATour/j3logging/main.go
+++ ATour/j3logging/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	utl "autilities"
+	"log"
+	"os"
 )
 
 var header = utl.Header{}
@@ -15,4 +17,26 @@ func main() {
 	 */
 	header.DisplayHeader("Showing Logging")
 
+	// Simply invoking functions like Println from the log package uses the standard logger, which is already pre-configured
+	// for reasonable logging output to os.Stderr. Additional methods like Fatal* or Panic* will exit the program after logging.
+	log.Println("standard logger")
+
+	// Loggers can be configured with flags to set their output format. By default, the standard logger has the log.Ldate
+	// and log.Ltime flags set, and these are collected in log.LstdFlags. We can change its flags to emit time with microsecond
+	// accuracy, for example.
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	log.Println("with micro")
+
+	// It also supports emitting the file name and line from which the log function is called.
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.Println("with file/line")
+
+	// It may be useful to create a custom logger and pass it around. When creating a new logger, we can set a prefix to
+	// distinguish its output from other loggers.
+	custlog := log.New(os.Stdout, "my:", log.LstdFlags)
+	custlog.Println("from mylog")
+
+	// We can set the prefix on existing loggers (including the standard one) with the SetPrefix method.
+	custlog.SetPrefix("ohmy:")
+	custlog.Println("from mylog")
 }
