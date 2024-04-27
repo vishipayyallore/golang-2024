e23406a873da48a6eca2588627948157cd0e446a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-27 14:05:51 +0530

    Update main.go

--- ATour/j2errors/main.go
+++ ATour/j2errors/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	utl "autilities"
+	"strconv"
 )
 
 var header = utl.Header{}
@@ -13,4 +14,15 @@ func main() {
 	 */
 	header.DisplayHeader("Showing Errors")
 
+	showErrorsDemo1("42")
+	showErrorsDemo1("A42")
+}
+
+func showErrorsDemo1(value string) {
+	i, err := strconv.Atoi(value)
+	if err != nil {
+		utl.PFmted("couldn't convert number: %v\n", err)
+		return
+	}
+	utl.PLine("Converted integer:", i)
 }
