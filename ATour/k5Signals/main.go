ae2db93b64b98a3809f0dd3906879a97a65d104c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-26 22:38:53 +0530

    Update main.go

--- ATour/k5Signals/main.go
+++ ATour/k5Signals/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	utl "autilities"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 var header = utl.Header{}
@@ -16,4 +19,25 @@ func main() {
 	 */
 	header.DisplayHeader("Showing Signals")
 
+	// Go signal notification works by sending os.Signal values on a channel. We’ll create a channel to receive these notifications. Note that this channel should be buffered.
+	sigs := make(chan os.Signal, 1)
+
+	// signal.Notify registers the given channel to receive notifications of the specified signals.
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	// We could receive from sigs here in the main function, but let’s see how this could also be done in a separate goroutine, to demonstrate a more realistic scenario of graceful shutdown.
+	done := make(chan bool, 1)
+
+	// This goroutine executes a blocking receive for signals. When it gets one it’ll print it out and then notify the program that it can finish.
+	go func() {
+		sig := <-sigs
+		utl.PLine()
+		utl.PLine(sig)
+		done <- true
+	}()
+
+	// The program will wait here until it gets the expected signal (as indicated by the goroutine above sending a value on done) and then exit.
+	utl.PLine("awaiting signal")
+	<-done
+	utl.PLine("exiting")
 }
